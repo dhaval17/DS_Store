f6a6c26183c9988fcf8c7cfd6ac92ef6c1528d52
Dhaval Chauhan <dhavalchauhan171994@gmail.com>
2015-12-21 13:43:16 +0530

    Added Type

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # DS_Store
 Python script to view contents of .DS_Store file
 
-Usage `python dsstore.py -t .DS_Store`
+Usage `python dsstore.py -p /dir/.DS_Store -t Iloc`
 
 **Requirements**
 * ds_store
 
 install : `pip install ds_store`
+
+* Types supported 
+	
+	`Iloc
+	bwsp
+	lsvp
+	lsvP 
+	icvp`
--- dsstore.py
+++ dsstore.py
@@ -3,14 +3,14 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-t", "--target", help="Path to the DS_Store file", required=True)
-
+parser.add_argument("-p", "--path", help="Path to the DS_Store file", required=True)
+parser.add_argument("-t", "--type", help="Type : Iloc, bwsp, lsvp, lsvP, icvp", default=100)
 args = parser.parse_args()
 
-dsstore = DSStore.open(args.target, 'r+')
+dsstore = DSStore.open(args.path, 'r+')
 for data in dsstore:
 	data = str(data)
 	entry = data.translate(None, "<>")
 	entry = entry.split(' ')
-	if(entry[1] == 'Iloc'):
+	if(entry[1] == args.type):
 		print(entry[0])
